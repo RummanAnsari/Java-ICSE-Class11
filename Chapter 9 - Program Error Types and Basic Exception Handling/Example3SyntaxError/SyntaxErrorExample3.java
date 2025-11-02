0c20308799d51946861b68aa94986571ee97ce79
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 14:47:02 +0530

    Solution 3 Commit

---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example3SyntaxError/SyntaxErrorExample3.java
@@ -0,0 +1,12 @@
+package Example3SyntaxError;
+
+public class SyntaxErrorExample3 {
+    public static void main(String[] args) {
+        int max;
+        // max = ;  // Missing right-hand side value
+
+        // Solution
+        max = 100;  // Assigned a value to max
+        System.out.println("Maximum value is: " + max);
+    }
+}
---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example3SyntaxError/SyntaxErrorExample3Solution.java
@@ -0,0 +1,9 @@
+package Example3SyntaxError;
+
+public class SyntaxErrorExample3Solution {
+        public static void main(String[] args) {
+        int max;
+        max = 100;  // Right-hand side value provided
+        System.out.println("Maximum value is: " + max);
+    }
+}
