6c7ef213676b4a953b84de9e9d5f4ba9135ceafd
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 14:44:09 +0530

    Commit solution

---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example2SyntaxError/SyntaxErrorExample2Solution.java
@@ -0,0 +1,11 @@
+package Example2SyntaxError;
+
+public class SyntaxErrorExample2Solution {
+    public static void main(String[] args) {
+        int I = 10;
+
+        if (I > 5) {  // Corrected expression
+            System.out.println("I is greater than 5");
+        }
+    }
+}
