97f102cefe46216a1aafeea46d29c80e0e81cae5
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 14:35:59 +0530

    commit done

--- Chapter 9 - Program Error Types and Basic Exception Handling/SyntaxErrorExample1.java
+++ Chapter 9 - Program Error Types and Basic Exception Handling/SyntaxErrorExample1.java
@@ -1,6 +1,10 @@
 class SyntaxErrorExample1 {
     public static void main(String[] args) {
-        String str = "This is a long string'; // Missing closing double quote
+         // String str = "This is a long string'; // Missing closing double quote
+
+         // Solution
+        String str = "This is a long string"; // Missing closing double quote...corrected
+       
         System.out.println(str);
     }
 }
