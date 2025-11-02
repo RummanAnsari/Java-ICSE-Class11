d29d9c8c9288f2a7d2a58d49c013db70f3b73cf8
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 15:28:14 +0530

    Example 7 added

---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example7SemanticErrorExample2/Example7SemanticErrorExample2.java
@@ -0,0 +1,11 @@
+package Example7SemanticErrorExample2;
+
+public class Example7SemanticErrorExample2 {
+    public static void main(String[] args) {
+       //  int a = "Hello";   // ❌ Incompatible data types
+       
+        // Solution
+        int a = 10;          // ✅ Correct data type
+       System.out.println(a);
+    }
+}
