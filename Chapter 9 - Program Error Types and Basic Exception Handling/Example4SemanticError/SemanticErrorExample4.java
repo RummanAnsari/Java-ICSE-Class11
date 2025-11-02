1da095b1f79264c88c59055d472395cd6550ce65
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 14:52:46 +0530

    Solution added

--- Chapter 9 - Program Error Types and Basic Exception Handling/Example4SemanticError/SemanticErrorExample4.java
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example4SemanticError/SemanticErrorExample4.java
@@ -2,7 +2,11 @@ package Example4SemanticError;
 
 public class SemanticErrorExample4 {
         public static void main(String[] args) {
-        int default = 10;  // 'default' is a reserved keyword in Java
-        System.out.println("Value: " + default);
+        // int default = 10;  // 'default' is a reserved keyword in Java
+        // System.out.println("Value: " + default);
+
+        // Solution
+        int defaultValue = 10;  // Renamed variable to a valid identifier       
+        System.out.println("Value: " + defaultValue);
     }
 }
---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/Example4SemanticError/SemanticErrorExample4Solution.java
@@ -0,0 +1,8 @@
+package Example4SemanticError;
+
+public class SemanticErrorExample4Solution {
+        public static void main(String[] args) {
+        int defaultValue = 10;  // Renamed variable to a valid identifier
+        System.out.println("Value: " + defaultValue);
+    }
+}
