1152ac9f6a6af9994c68f6b7d5621498c6d576aa
RummanAnsari <rummanansari6@gmail.com>
2025-11-02 14:29:16 +0530

    Code added

---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/SyntaxErrorExample1.java
@@ -0,0 +1,6 @@
+class SyntaxErrorExample1 {
+    public static void main(String[] args) {
+        String str = "This is a long string'; // Missing closing double quote
+        System.out.println(str);
+    }
+}
---
+++ Chapter 9 - Program Error Types and Basic Exception Handling/SyntaxErrorExample1Solution.java
@@ -0,0 +1,7 @@
+class SyntaxErrorExample1Solution {
+    public static void main(String[] args) {
+        String str = "This is a long string"; // Corrected version
+        System.out.println(str);
+    }
+}
+
