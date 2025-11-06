e7ee68f63c580cc8d46c7579e4183949893e6e4a
RummanAnsari <rummanansari6@gmail.com>
2025-11-06 20:30:33 +0530

    commit

---
+++ Chapter 6 - Flow of Control/EmptyLoopExample.java
@@ -0,0 +1,11 @@
+
+public class EmptyLoopExample {
+    public static void main(String[] args) {
+        int j;
+
+        // Empty for loop
+        for (j = 20; j >= 0; j--);
+        
+        System.out.println("Loop completed, final value of j = " + j);
+    }
+}
