f1fee7994d07a2a4fce2392ebc5e5de821ac418f
RummanAnsari <rummanansari6@gmail.com>
2025-11-29 21:59:31 +0530

    Commit code

---
+++ Chapter 09 - Program Error Types and Basic Exception Handling/Example029InputMismatchExceptionDemo/InputMismatchExceptionDemo.java
@@ -0,0 +1,34 @@
+package Example029InputMismatchExceptionDemo;
+
+import java.util.*;
+
+public class InputMismatchExceptionDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean continueInput = true;
+
+        do {
+            try {
+                System.out.print("Enter an integer: ");
+                int number = input.nextInt();
+
+                // Display the result
+                System.out.println(
+                    "The number entered is " + number);
+
+                continueInput = false;
+            }
+            catch (InputMismatchException ex) {
+                System.out.println("Try again. (" +
+                    "Incorrect input: an integer is required)");
+                input.nextLine(); // Discard input
+            }
+        } while (continueInput);
+    }
+}
+
+// Output Example:
+// Enter an integer: ten    
+// Try again. (Incorrect input: an integer is required)
+// Enter an integer: 10 
+// The number entered is 10
